f23b2fccee96c2ac3721e5609c07ef9bb981191f
gabrielnerys <gabe.nery@live.com>
2022-06-14 15:57:11 -0300

    feat: Add Basic Configurations and Fonts

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header/Header';
+import Portfolio from './components/Portfolio/Portfolio';
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Portfolio />
+    </>
+  );
+}
+
+export default App;
